6c382e42fc9ba65b35159039ca77bafe35c26760
Joison Júnior <140748196+JoisonJr@users.noreply.github.com>
2025-10-01 21:22:37 -0300

    exemplo.py atualizado com o método de Newton

--- Trabalho1/exemplo.py
+++ Trabalho1/exemplo.py
@@ -14,10 +14,9 @@ def df(x):
 tolerancia = 1e-6
 max_iteracoes = 100
 
+"""
 # Usar a nova função para definir os valores de 'a' e 'b'
 a, b = encontrar_intervalo(f, ponto_inicial=1, passo=0.5)
-
-################################################################################################################################
 
 # --- Execução e Visualização ---
 if a is not None and b is not None:
@@ -48,3 +47,31 @@ if a is not None and b is not None:
         plt.legend()
         plt.grid(True)
         plt.show()
+"""
+
+raiz, iteracoes = newton_raphson(f, df, 3, tol=tolerancia, max_iter=max_iteracoes)
+
+if raiz is not None:
+    # Gerar pontos para plotar a curva da função
+    x_vals = np.linspace(min(iteracoes) - .5, max(iteracoes) + .5, 400)
+    y_vals = f(x_vals)
+
+    plt.figure(figsize=(12, 15))
+    plt.plot(x_vals, y_vals, label='f(x) = $x^3 - 3$', color='blue')
+    
+    # Adicionar a linha do eixo x (onde f(x) = 0)
+    plt.axhline(0, color='black', linestyle='--')
+    
+    # Plotar os pontos de cada iteração para mostrar a convergência
+    plt.plot(iteracoes, [f(p) for p in iteracoes],  
+                'ro', label='Iterações do Método')
+    
+    # Marcar a raiz encontrada
+    plt.plot(raiz, f(raiz), 'go', markersize=10, label='Raiz Encontrada')
+
+    plt.title('Falsa posição', fontsize=16)
+    plt.xlabel('Eixo X')
+    plt.ylabel('f(x)')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
